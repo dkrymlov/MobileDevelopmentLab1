e931e53083b16bdfa48dc3b0c4399a41222549cc
dkrymlov <91798748+dkrymlov@users.noreply.github.com>
2023-09-11 11:00:35 +0300

    Initial Commit

---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by Даниил Крымлов on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
